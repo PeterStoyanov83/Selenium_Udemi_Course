54e887baaf479a38071a5195c6767d024928d9ab
peterstoyanov <peterstoyanov83@gmail.com>
2023-08-10 20:28:34 +0200

    committing to study

--- python_selenium_course_material/PYTHON_SECTION/Classes/exercises/create_calculator_class_exercise.py
+++ python_selenium_course_material/PYTHON_SECTION/Classes/exercises/create_calculator_class_exercise.py
@@ -8,3 +8,26 @@ You can have as many methods as you like but here are few suggestions.
 * method to divide two numbers and return the result (first number divided by second number)
 
 """
+
+
+class Calculator:
+
+    def add(self, a, b) -> None:
+        return a + b
+
+    def substract(self, a, b) -> None:
+        return a - b
+
+    def multiply(self, a, b) -> None:
+        return a * b
+
+    def divide(self, a, b) -> None:
+        return a / b
+
+
+calculator = Calculator()
+print(calculator.add(2, 3))
+print(calculator.substract(2, 3))
+print(calculator.multiply(2, 3))
+print(calculator.divide(2, 4))
+
